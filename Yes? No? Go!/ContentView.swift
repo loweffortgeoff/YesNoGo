c8ab27974ee7e4f1e6187311fd8faf798529b3ed
Geoffrey Silva <geoff@loweffortmedia.com>
2025-11-01 18:39:34 -0400

    Initial Commit

---
+++ Yes? No? Go!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yes? No? Go!
+//
+//  Created by Geoffrey Silva on 11/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yes? No? Go!/Yes__No__Go_App.swift
@@ -0,0 +1,17 @@
+//
+//  Yes__No__Go_App.swift
+//  Yes? No? Go!
+//
+//  Created by Geoffrey Silva on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Yes__No__Go_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
